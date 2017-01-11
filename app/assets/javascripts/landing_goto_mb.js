c84c20a7b746e4e590f263f612995d773d0d4999
Vitaliy Duvalko <vitaliy.duvalko@gmail.com>
2017-01-11 21:04:33 +0200

    last

---
+++ app/assets/javascripts/landing_goto_mb.js
@@ -0,0 +1,7 @@
+$(function(){
+	alert(document.documentElement.clientWidth);
+	alert(document.documentElement.clientHeight);
+	if (!navigator.userAgent.match(/Iphone/i)) {
+		$('#mb-wrapper-landing').attr('id', 'mb-wrapper-landing-android');
+	}
+})
