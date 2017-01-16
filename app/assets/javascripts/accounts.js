1782e0f34d2f34aa3a5b0b64c44108b48ac83c4e
Vitaliy Duvalko <vitaliy.duvalko@gmail.com>
2017-01-16 17:10:17 +0200

    autoprefix

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -1,3 +1,12 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).on('turbolinks:load', function() {
+	$(".mb-burger").click(function(){
+		$(this).toggleClass("is-active");
+		$('.mb-menu').toggleClass("active");
+	});
+
+	$('.mb-menu li a').click(function(){
+		var path = $(this).attr('href');
+		$(this).addClass("active");
+		Turbolinks.clearCache();
+	});
+});
