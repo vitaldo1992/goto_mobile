208083025f3fe2b61d980ecb5983d2ae28190be4
Vitaliy Duvalko <vitaliy.duvalko@gmail.com>
2017-01-30 15:55:38 +0200

    many_bugs

--- app/assets/javascripts/landing_goto_mb.js
+++
@@ -1,7 +0,0 @@
-$(document).on('turbolinks:load', function() {
-	// alert(document.documentElement.clientWidth);
-	// alert(document.documentElement.clientHeight);
-	if (!navigator.userAgent.match(/Iphone/i)) {
-		$('#mb-wrapper-landing .mb-wrap').addClass('android-wrap');
-	}
-});
